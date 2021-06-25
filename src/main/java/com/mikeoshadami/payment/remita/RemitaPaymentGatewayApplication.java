fcaeab2f78c79c4c4c10ecc9e47c846c510c45bf
Mike Oshadami <oshadami@systemspecs.com.ng>
2021-06-25 14:18:23 +0100

    Adding Springboot Configurations

---
+++ src/main/java/com/mikeoshadami/payment/remita/RemitaPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mikeoshadami.payment.remita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitaPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitaPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikeoshadami/payment/remita/RemitaPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikeoshadami.payment.remita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitaPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
